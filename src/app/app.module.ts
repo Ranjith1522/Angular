aac6c0dc43ee1de8c4597c08a5da3385ebc7ed4e
password <ranjithkankati2@gmail.com>
2025-03-29 10:15:10 +0530

    Added routing to the Dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CentralComponent } from './central/central.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
 
+
+const routes : Routes = [
+  {path:'', redirectTo:'dashboard',pathMatch : 'full'},
+  {path:'Home',component : HomeComponent},
+  {path:'Central', component : CentralComponent},
+  {path:'Services',component : ServicesComponent},
+  {path:'About',component : AboutComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent,
+    CentralComponent,
+    ServicesComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
